e16d7afcd9e516dfb7d1740a852e5e25607fe7d1
tatangharyadi <tatang.haryadi@gmail.com>
2021-06-19 10:50:28 +0700

    refactor to configService

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,5 @@
-import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
@@ -13,19 +11,6 @@ import { JwtModule } from '@nestjs/jwt';
                 secret: configService.get('JWT_SECRET'),
                 signOptions: {
                     expiresIn: configService.get('JWT_EXPIRY'),
-                },
-            }),
-        }),
-        MailerModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
-                transport: {
-                    host: configService.get('SMTP_HOST'),
-                    port: configService.get('SMTP_PORT'),
-                },
-                defaults: {
-                    from: configService.get('NO_REPLY'),
                 },
             }),
         }),
--- src/common/root.module.ts
+++ src/common/root.module.ts
@@ -1,4 +1,5 @@
 import * as Joi from '@hapi/joi';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
@@ -18,9 +19,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
                 type: 'postgres',
-                url: process.env.POSTGRES_URL,
+                url: configService.get('POSTGRES_URL'),
                 autoLoadEntities: true,
                 synchronize: true,
+            }),
+        }),
+        MailerModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                transport: {
+                    host: configService.get('SMTP_HOST'),
+                    port: configService.get('SMTP_PORT'),
+                },
+                defaults: {
+                    from: configService.get('NO_REPLY'),
+                },
             }),
         }),
     ],
